11a1e3632b2220e833aacef45acf6ead06f47205
Vitor Hugo <123006549+vitorhugo03@users.noreply.github.com>
2023-06-08 12:41:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let chave = "d784c0a25984ee1aedd8d2915c35a439"
+
+function colocarNaTela(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + chave + "&lang=pt_br" + "&units=metric")
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+function cliqueNoBotao(){
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
